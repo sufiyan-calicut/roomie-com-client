dbc2e82e2246c16cba8bd06422765063da4e3f5a
sufiyan-calicut <115002412+sufiyan-calicut@users.noreply.github.com>
2023-05-19 15:01:41 +0530

    Update hotelApi.js

--- src/api/hotelApi.js
+++ src/api/hotelApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const hotelApi = axios.create({
-  baseURL: 'http://localhost:4000/api/hotel',
+  baseURL: 'https://roomie-server.onrender.com/api/hotel',
 });
 
 hotelApi.interceptors.request.use(
