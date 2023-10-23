925285e7fba52956f268a6ebda049bec353f5167
sufiyan-calicut <115002412+sufiyan-calicut@users.noreply.github.com>
2023-10-23 21:59:35 +0530

    bugs fixed

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 export const userApi = axios.create({
-  // baseURL: 'http://localhost:4000/api/user',
-  baseURL: 'https://roomie-server.onrender.com/api/user',
+  baseURL: 'http://localhost:4000/api/user',
+  // baseURL: 'https://roomie-server.onrender.com/api/user',
 });
 userApi.interceptors.request.use(
   (config) => {
